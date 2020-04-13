cf24b2cac5da24b016cde13f21a0822b155e5762
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-04-13 13:19:07 +0200

    chage hasRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Link, HashRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
 import Main from './components/main';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
     <div className="demo-big-content">
-    <HashRouter basename='/'>
+
       <Layout>
           <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/"> MyPortfolio </Link>} scroll>
               <Navigation>
@@ -31,7 +31,7 @@ function App() {
               <Main/>
           </Content>
       </Layout>
-    </HashRouter>
+
 </div>
 
 
