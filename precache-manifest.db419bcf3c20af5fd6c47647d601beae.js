9a7cc6cd1b4640c1f2e088eedfeaa0ff3bea642d
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-04-13 16:11:03 +0200

    Updates

---
+++ precache-manifest.db419bcf3c20af5fd6c47647d601beae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea98fc55b1af1034a01f4960d12e8d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fc0341a21e30c8f9ea",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "3c7cec6dc95ba79a6eb7",
+    "url": "/static/css/main.72022d82.chunk.css"
+  },
+  {
+    "revision": "43fc0341a21e30c8f9ea",
+    "url": "/static/js/2.1cdfde85.chunk.js"
+  },
+  {
+    "revision": "3c7cec6dc95ba79a6eb7",
+    "url": "/static/js/main.ca54b03f.chunk.js"
+  },
+  {
+    "revision": "2013ba59ba4a1135a1a1",
+    "url": "/static/js/runtime-main.9f488a65.js"
+  }
+]);
